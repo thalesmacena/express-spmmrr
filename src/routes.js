8123bf5b8750ae672998bccc430639e285212cd3
Thales Macena <thalesmacena@gmail.com>
2020-11-18 19:35:49 -0300

    add multer to routes

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,12 @@
 import { Router } from 'express';
+import multer from 'multer';
+import multerConfig from './config/multer';
 
 const routes = new Router();
+const upload = multer(multerConfig);
+
+routes.get('/', (req, res) => {
+  return res.json({ message: 'The index of the application' });
+});
 
 export default routes;
